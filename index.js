27af7e1cb420769e339994739ff3806909043f9f
theGreatKang98 <srouc159@gamil.com>
2022-11-16 23:41:04 +0900

    todo: 빠르게 커서 뺄 시 getSrc 한번 더 호출함

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ async function getSrc(link) {
     const response = await fetch(link);
     const text = await response.text();
     const html = new DOMParser().parseFromString(text, 'text/html');
-    console.log('func called')
-    return html.querySelector('.write_div').querySelector('img')?.src;
+    console.log('func called');
+    return html.querySelector('.write_div')?.querySelector('img')?.src;
 }
 
-const lists = document.querySelectorAll('.gall_tit');
+const lists = document.querySelectorAll('.gall_tit a');
 lists.forEach(list => {
     list.addEventListener('mouseover', async () => {
-        const link = list.querySelector('a').href;
+        const link = list.href;
         if (list.querySelector('.icon_pic')) {
             const src = await getSrc(link);
+            const img = document.createElement('img');
             if (src) {
-                const img = document.createElement('img');
                 img.id = 'loadedImg';
                 img.src = src;
                 img.style.position = 'absolute';
-                img.style.width = `${150}px`;
-                img.style.height = `${150}px`;
+                img.style.width = '30%';
                 img.style.zIndex = 10;
                 list.after(img);
             }
         }
     });
+    
     list.addEventListener('mouseout', () => {
         const loadedImg = document.querySelectorAll('#loadedImg');
         loadedImg.forEach(img => img.remove());
