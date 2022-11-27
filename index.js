e61ee00583abd6536975ed8fb4e3836c3ec75abf
theGreatKang98 <srouc159@gamil.com>
2022-11-27 18:02:57 +0900

    timer ver

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 
-async function getSrc(link) {
+const getSrc = async (link) => {
     const response = await fetch(link);
     const text = await response.text();
     const html = new DOMParser().parseFromString(text, 'text/html');
     return html.querySelector('.write_div')?.querySelector('img')?.src;
+}
+
+const timer = {
+    async callTimer(callback,link,list) {
+        this.timerID =  setTimeout(async () => {
+            const src = await callback(link);
+            if (src) {
+                const img = document.createElement('img');
+                img.id = link;
+                img.className = 'loadedImg';
+                img.src = src;
+                img.style.position = 'absolute';
+                img.style.width = '30%';
+                img.style.zIndex = 10;
+                list.after(img);
+            }
+        }, 200);
+    },
+
+    resetTimer () {
+        clearTimeout(this.timerID);
+    }
 }
 
 
@@ -11,22 +33,20 @@ const lists = document.querySelectorAll('.gall_tit a');
 lists.forEach(list => {
     list.addEventListener('mouseover', async () => {
         const link = list.href;
-        if (!list.querySelector('.icon_txt')) {
-            const src = await getSrc(link);
-            const img = document.createElement('img');
-            if (src) {
-                img.id = 'loadedImg';
-                img.src = src;
-                img.style.position = 'absolute';
-                img.style.width = '30%';
-                img.style.zIndex = 10;
-                list.after(img);
-            }
+        const loadedImgs = Array.from(document.querySelectorAll('.loadedImg'));
+        const filteredImg = loadedImgs?.filter(i => i.id === link);
+        if (filteredImg[0]) {
+            filteredImg[0].style.visibility = 'visible';
+        } else if (!list.querySelector('.icon_txt')) {
+             timer.callTimer(getSrc,link,list);
         }
     });
-    
+
     list.addEventListener('mouseout', () => {
-        const loadedImg = document.querySelector('#loadedImg');
-        loadedImg?.remove();
+        const loadedImgs = document.querySelectorAll('.loadedImg');
+        loadedImgs?.forEach(i => i.style.visibility = 'hidden');
+        timer.resetTimer();
     });
 })
+
+clear() 
